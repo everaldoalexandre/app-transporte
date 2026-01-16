ec1aa0f7999217dd4ded7318fd4de2cc723a198b
Everaldo Alexandre <everaldo_lol@hotmail.com>
2026-01-16 10:02:55 -0300

    att

--- src/app/reset-password/page.tsx
+++ src/app/reset-password/page.tsx
@@ -1,5 +1,4 @@
 export const dynamic = "force-dynamic";
-("use client");
 
 import { MenuNavigationCompleto } from "@/components/MenuNavbar";
 import ResetPasswordPage from "@/components/NovaSenha";
